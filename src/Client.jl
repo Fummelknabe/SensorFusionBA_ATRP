6cb7e2d9465165d98379886cced86ee145a9cfc2
martin.rolfs <martin.rolfs@student.uni-luebeck.de>
2022-05-16 16:17:02 +0200

    Modified connectToJetson function

--- src/Client.jl
+++ src/Client.jl
@@ -1,17 +1,25 @@
 using Sockets
 
 export HOST, PORT
-const HOST = "10.42.0.1"; 
+const HOST = ip"192.168.0.103"; #"10.42.0.1"
       PORT = 2222
 
-export connectToJetson
-function connectToJetson(ip::String, port::Integer)
+
+export connectToJetsoncl
+function connectToJetson(ip::IPv4, port::Integer)
     try
         clientSocket = connect(ip, port)
-        print(clientSocket)
+
+        @async while !eof(clientSocket)
+            readline(clientSocket, keep=true)
+            println(clientSocket, "Test Message!")
+            println(stdout, "Are we here3?")
+        end
     catch error
         if isa(error, Base.IOError)
             @warn "Connection to Jetson Timed Out!"
+        else
+            print(error)
         end
     end
 end
