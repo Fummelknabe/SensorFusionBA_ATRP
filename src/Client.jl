86c9de11531d2bc039b62fd0e955edd892dd0ef9
martin.rolfs <martin.rolfs@student.uni-luebeck.de>
2022-05-23 16:16:47 +0200

    Added basic functionality to connect via tcp

--- src/Client.jl
+++ src/Client.jl
@@ -1,26 +1,62 @@
 using Sockets
+using PyCall
+
+pysockets = pyimport("socket")
 
 export HOST, PORT
-const HOST = ip"192.168.0.103"; #"10.42.0.1"
+const HOST = "192.168.0.103"; #"10.42.0.1"
       PORT = 2222
 
+# Create a python socket
+export pysocket
+pysocket = pysockets.socket(pysockets.AF_INET, pysockets.SOCK_STREAM)
+pysocket.settimeout(20)
 
-export connectToJetsoncl
-function connectToJetson(ip::IPv4, port::Integer)
+export connectToJetson
+"""
+Connects to the Jetson
+
+# Arguments
+- `ip::String`: The IP-Address as string to connect to.
+- `port::Integer`: The Port of the software to connect to.
+
+# Returns
+- `boolean`: True if connection to a valid controller was succesful.
+"""
+function connectToJetson(ip::String, port::Integer)
     try
-        clientSocket = connect(ip, port)
-
-        @async while !eof(clientSocket)
-            readline(clientSocket, keep=true)
-            println(clientSocket, "Test Message!")
-            println(stdout, "Are we here3?")
-        end
+        pysocket.connect((ip, port))
     catch error
         if isa(error, Base.IOError)
             @warn "Connection to Jetson Timed Out!"
         else
             print(error)
         end
+
+        pysocket.close()
+        return false
     end
+
+    # Connected to something -----
+    data = pysocket.recv(1024)
+    println(data)
+
+    pysocket.send(b"atsv_controller")
+
+    return data == "atsv_brain_welcome"
 end
 
+"""
+This function sends and receives data over the established tcp connection.
+
+# Arguments 
+- `data::String`: The data to send as String.
+
+# Returns
+- `String`: The data the sockets receives.
+"""
+function sendAndRecvData(data::String)
+    pysocket.send(codeunits(data))
+
+    return pysocket.recv(1024)
+end
--- src/Main.jl
+++ src/Main.jl
@@ -1,11 +1,18 @@
 include("Client.jl")
+
 """
 
 This is the starting point of the program.
 """
 function main()
     println("The program is started! \nTrying to connect to Jetson:")
-    connectToJetson(HOST, PORT)
+    
+    if connectToJetson(HOST, PORT)
+        println("Connection established!")
+    end
+
+    # Send a few commands for testing purposes:
+    println(sendAndRecvData("Hallo Jetson!"))
 end
 
 main()
