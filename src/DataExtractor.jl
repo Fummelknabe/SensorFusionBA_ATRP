603b7c58b4dba3fc4356b23a67de1f8bd53ddb2e
martin.rolfs <martin.rolfs@student.uni-luebeck.de>
2022-05-31 19:33:50 +0200

    Parsed data to correct Data Type

--- src/DataExtractor.jl
+++ src/DataExtractor.jl
@@ -1,15 +1,19 @@
+using JSON
+
 export PositionalData
 """
 Struct to store the data acquired by the AT-RP
 """
-struct PositionalData
+mutable struct PositionalData
     steerAngle::Integer
     maxSpeed::Integer
     sensorSpeed::Float32
     cameraPos::Vector{Float32}
     imuGyro::Vector{Float32}
     imuAcc::Vector{Float32}
-    imuMag::Vector{Float32}
+    imuMag::Float32
+
+    PositionalData() = new(0, 0, 0.0, [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 0.0)
 end
 
 export extractData
@@ -32,6 +36,7 @@ function extractData(data::String)
         #throw(MethodError(extractData, "Missing data from AT-SV => Cannot Extract Data"))
     end
 
+    """
     println("Status: " * splitted[1] * "\n" *
             "Set Speed: " * splitted[2] * "\n" *
             "Steer Angle: " * splitted[3] * "\n" *
@@ -40,4 +45,15 @@ function extractData(data::String)
             "IMU Acceleration: " * splitted[6] * "\n" *
             "IMU Angular Rate: " * splitted[7] * "\n" *
             "IMU Magnetic Field: " * splitted[8])
+            """
+    posData = PositionalData()
+    posData.maxSpeed = parse(Int, splitted[2])
+    posData.steerAngle = parse(Int, splitted[3])
+    posData.sensorSpeed = parse(Float32, splitted[4])
+    posData.cameraPos = parse.(Float32, split(chop(splitted[5]; head=1, tail=1), ','))
+    posData.imuAcc = parse.(Float32, split(chop(splitted[6]; head=1, tail=1), ','))
+    posData.imuGyro = parse.(Float32, split(chop(splitted[7]; head=1, tail=1), ','))
+    posData.imuMag = parse(Float32, splitted[8])
+
+    println(posData)
 end
