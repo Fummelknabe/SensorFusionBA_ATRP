c374b0a873c0acbc363c34f78b36e3a390c32671
Fummelknabe <rolfs.martin@googlemail.com>
2022-10-15 10:49:37 +0200

    Added comments and new opengl camera rotation

--- src/Camera.jl
+++ src/Camera.jl
@@ -25,7 +25,17 @@ function getViewMatrix(cam::Camera)
     return viewMatrix
 end
 
-function rotateAroundAxis(degrees::Float64, axis::Vector{Float32})
+"""
+This method returns the rotational matrix in homogonous coordinates. 
+
+# Arguments
+`degrees::Float64`: The degrees to rotate.
+`axis::Vector{Float32}`: The axis to rotate around. 
+
+# Returns
+`Matrix{Float64}`: 4x4 rotational matrix that describes the specified rotational behaviour.
+"""
+function rotateAroundAxis(degrees::Float64, axis::Vector{Float64})
     ncos = 1 - cosd(degrees)
     sin = sind(degrees)
     cos = cosd(degrees)
@@ -43,12 +53,42 @@ function checkCameraMovement(mousePos::Vector{Float64}, cam::Camera)
         camX = cam.speed * difVector[1]
         camY = cam.speed * difVector[2]
 
-        sideAxis = cross(cam.up, cam.position);
-        transformMatrix = rotateAroundAxis(camX, cam.up) * rotateAroundAxis(camY, normalize!(sideAxis))
+        translationVector = vcat(-cam.position + [camX, camY, 0.0], 0.0)
+        baseTransDist = norm(cam.position) # This should be 2
+        sideAxis = cross(cam.up, cam.position)
 
+        angleBetweenVecs(a, b) = acosd(clamp(a⋅b/(norm(a)*norm(b)), -1, 1))
+        # angle between x axis and side axis and z axis and up vector
+        θ = angleBetweenVecs([1.0, 0.0, 0.0], sideAxis)
+        η = angleBetweenVecs([0.0, 1.0, 0.0], cam.up)
+
+        transformMatrix = rotateAroundAxis(θ, [0.0, 1.0, 0.0]) * rotateAroundAxis(η, [1.0, 0.0, 0.0]) * hcat(Matrix(I,4,3), translationVector)
+
+        # Modify camera position
         cam.up = normalize!(deleteat!(transformMatrix * push!(cam.up, 1.0), 4))
 	    cam.position = deleteat!(transformMatrix * push!(cam.position, 1.0), 4);
+
+        # undo matrix transformation
+        invTransformMatrix = hcat(Matrix(I,4,3), baseTransDist*normalize!(translationVector)) * rotateAroundAxis(η, [1.0, 0.0, 0.0]) * rotateAroundAxis(θ, [0.0, 1.0, 0.0])
+
+        cam.up = normalize!(deleteat!(invTransformMatrix * push!(cam.up, 1.0), 4))
+	    cam.position = deleteat!(invTransformMatrix * push!(cam.position, 1.0), 4);
+
+        # OLD CALCULATION
+        # calculate side axis
+        #sideAxis = cross(cam.up, cam.position);
+        #transformMatrix = rotateAroundAxis(camX, cam.up) * rotateAroundAxis(camY, normalize!(sideAxis))
+
+        #cam.up = normalize!(deleteat!(transformMatrix * push!(cam.up, 1.0), 4))
+	    #cam.position = deleteat!(transformMatrix * push!(cam.position, 1.0), 4);
     end 
+    
+    #= 
+    it could be tried to transform the camera coordinate in the global coordinate frame in the 
+    origin. Then translate camera camX vertically and camY laterally. Remove the affects of the transform
+    by mutiplying with the inverse of the transform matrix.
+    Retrace steps and find wrong thoughts
+    =#
 
     global oldMousePosition = mousePos
 end
